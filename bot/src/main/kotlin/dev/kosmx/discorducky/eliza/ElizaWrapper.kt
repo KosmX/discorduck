a3498d28aaf8fa260a2dc725f734fe6064e473b1
KosmX <kosmx.mc@gmail.com>
2025-04-01 13:41:54 +0000

    Add ELIZABOT.js

---
+++ bot/src/main/kotlin/dev/kosmx/discorducky/eliza/ElizaWrapper.kt
@@ -0,0 +1,50 @@
+package dev.kosmx.discorducky.eliza
+
+import org.graalvm.polyglot.Context
+import java.lang.ref.Cleaner
+
+class ElizaWrapper(noRandom: Boolean = false): AutoCloseable {
+    private val context = Context.create()
+
+
+    companion object {
+        private val cleaner = Cleaner.create()
+        val elizaHelperJS = """
+            
+        """.trimIndent()
+
+        val elizajs = ElizaWrapper::class.java.getResourceAsStream("/eliza/elizabot.js").bufferedReader().use { it.readText() }
+        val elizadata = ElizaWrapper::class.java.getResourceAsStream("/eliza/elizadata.js").bufferedReader().use { it.readText() }
+    }
+
+    init {
+        context.eval("js", elizadata)
+        context.eval("js", elizajs)
+
+    }
+    val closeable = cleaner.register(this, Resource(context))!!
+
+    private val elizaBot = context.eval("js", "new ElizaBot($noRandom)")
+
+
+    override fun close() {
+        closeable.clean()
+    }
+
+
+    fun transform(inputString: String): String {
+        return elizaBot.invokeMember("transform", inputString).asString()
+    }
+
+    var memSize: Int
+        get() = elizaBot.getMember("memSize").asInt()
+        set(value) {
+            elizaBot.putMember("memSize", value)
+        }
+
+    private data class Resource(val context: Context): Runnable {
+        override fun run() {
+            context.close()
+        }
+    }
+}
